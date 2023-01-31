90929a9f69f9f15cfb855aae2c6157462dc562d5
Danny <dannym.mejia@gmail.com>
2023-01-31 07:05:56 +0100

    N2 ex1 finalizado

---
+++ entrega1_1.js
@@ -0,0 +1,12 @@
+//Nivel 1
+//Ex. 1
+var user = "pepe";
+function userName(name) {
+  console.log(name);
+}
+userName(user);
+//Nivel 2
+//Ex. 1
+var name = "Danny";
+var lastName = "Mejia";
+console.log(`${name} ${lastName}`);
