bad644caa6de120fe96b84b3bf7ef9bb66185f1c
Danny Mejía <dannym.mejia@gmail.com>
2023-01-31 09:38:37 +0100

    acabado hasta N3 ex1

--- entrega1_1.js
+++ entrega1_1.js
@@ -1,12 +1,34 @@
 //Nivel 1
 //Ex. 1
-var user = "pepe";
+let user = "pepe";
 function userName(name) {
   console.log(name);
 }
 userName(user);
 //Nivel 2
 //Ex. 1
-var name = "Danny";
-var lastName = "Mejia";
+let name = "Danny";
+let lastName = "Mejia";
 console.log(`${name} ${lastName}`);
+//Ex 2.
+function value(){
+  return 5;
+}
+console.log(`el valor es ${value()}`)
+//Nivel 3
+//Ex. 1
+let arr = [];
+for(let i = 0; i < 9; i ++){
+  arr.push(function(){
+    console.log(i);
+  })
+}
+for(let i = 0; i < arr.length; i++){
+  console.log(i)
+}
+/*
+const  arr = function(){
+  for
+  return i;
+}*/
+//Ex. 2
