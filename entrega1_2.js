ba04e049db6c81e2cfb73e6d9465964ff4be6bc6
Danny Mejía <dannym.mejia@gmail.com>
2023-02-01 09:39:36 +0100

    Creacion de todos los archivos del Sprint 1

--- entrega1_2.js
+++ entrega1_2.js
@@ -5,7 +5,22 @@ let nameTwo = "Olaf";
 ((n, m)=>{
     console.log(n + " " + m)
 })(nameOne,nameTwo);
-//Ex 2.
+//Nivel 2
+//Ex. 1
 const a = (nombre)=> ({nombre : nombre});
 let b = a("danny");
 console.log(b.nombre);
+//Ex. 2
+class Persona {
+    constructor(name){
+        this.name = name;
+    }
+    dirNom(){
+        console.log(this.name);
+    }
+}
+let user = new Persona("Jose");
+user.dirNom();
+//Nivel 3
+//Ex 1.
+
