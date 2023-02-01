19eb270e84f28ca7270c845d20d5db991fa2572b
Danny Mejia <dannym.mejia@gmail.com>
2023-02-01 13:07:11 +0100

    N3 Ex1 Acabado

--- entrega1_2.js
+++ entrega1_2.js
@@ -23,4 +23,36 @@ let user = new Persona("Jose");
 user.dirNom();
 //Nivel 3
 //Ex 1.
+class Abstract{
+    constructor() {
+        if(new.target == Abstract){
+            throw new TypeError("Cannot construct instances directly");
+        }  
+    }
+}
+const ab = new Abstract();
+console.log(ab);
+class TypeOne extends Abstract{
+    constructor(){
+        super();
+    }
+}
+class TypeTwo extends Abstract{
+    constructor() {
+        super();
+    }
+}
+function builder(type){
+    switch(type){
+        case "type1":
+            return new TypeOne();
+        case "type2":
+            return new TypeTwo();
+        default:
+            throw new Error("Invalid type");
+    }
+    return new Abstract();
+}
+const objOne = builder("type1");
+console.log(objOne);
 
