91230cc5d544362280aff1c1b0aa9990bc1bb392
Danny Mejia <dannym.mejia@gmail.com>
2023-02-02 12:54:32 +0100

    Codigo formateado

--- entrega1_1.js
+++ entrega1_1.js
@@ -11,24 +11,22 @@ let nombre = "Danny";
 let lastName = "Mejia";
 console.log(`${nombre} ${lastName}`);
 //Ex 2.
-function value(){
+function value() {
   return 5;
 }
-console.log(`el valor es ${value()}`)
+console.log(`el valor es ${value()}`);
 //Nivel 3
 //Ex. 1
 let arr = [];
-for(let i = 0; i < 10; i++){
-  arr.push(()=>{
-   for(let i = 0; i < 10; i++){
+for (let i = 0; i < 10; i++) {
+  arr.push(() => {
+    for (let i = 0; i < 10; i++) {
       console.log(i);
     }
-  })
+  });
 }
-arr.forEach(element =>
-  element()
-);
+arr.forEach((element) => element());
 //Ex. 2
-const naming = (function(nombre) {
-  console.log(nombre); 
+const naming = (function (nombre) {
+  console.log(nombre);
 })(user);
--- entrega1_2.js
+++ entrega1_2.js
@@ -2,57 +2,57 @@
 //Ex. 1
 let nameOne = "Juan";
 let nameTwo = "Olaf";
-((n, m)=>{
-    console.log(n + " " + m)
-})(nameOne,nameTwo);
+((n, m) => {
+  console.log(n + " " + m);
+})(nameOne, nameTwo);
 //Nivel 2
 //Ex. 1
-const a = (nombre)=> ({nombre : nombre});
+const a = (nombre) => ({ nombre: nombre });
 let b = a("danny");
 console.log(b.nombre);
 //Ex. 2
 class Persona {
-    constructor(name){
-        this.name = name;
-    }
-    dirNom(){
-        console.log(this.name);
-    }
+  constructor(name) {
+    this.name = name;
+  }
+  dirNom() {
+    console.log(this.name);
+  }
 }
 let user = new Persona("Jose");
 user.dirNom();
 //Nivel 3
 //Ex 1.
-class Vehiculo{
-    constructor(ruedas) {
-        this.ruedas = ruedas;
-        if(new.target == Vehiculo){
-            throw new TypeError("Cannot construct instances directly");
-        }
+class Vehiculo {
+  constructor(ruedas) {
+    this.ruedas = ruedas;
+    if (new.target == Vehiculo) {
+      throw new TypeError("Cannot construct instances directly");
     }
+  }
 }
-class Coche extends Vehiculo{
-    constructor(ruedas){
-        super(ruedas);
-    }
+class Coche extends Vehiculo {
+  constructor(ruedas) {
+    super(ruedas);
+  }
 }
-class Moto extends Vehiculo{
-    constructor(ruedas) {
-        super(ruedas);
-    }
+class Moto extends Vehiculo {
+  constructor(ruedas) {
+    super(ruedas);
+  }
 }
-function builder(ruedas, type){
-    switch(type){
-        case "type1":
-            return new Coche(ruedas);
-        case "type2":
-            return new Moto(ruedas);
-        default:
-            throw new Error("Invalid type");
-    }
+function builder(ruedas, type) {
+  switch (type) {
+    case "type1":
+      return new Coche(ruedas);
+    case "type2":
+      return new Moto(ruedas);
+    default:
+      throw new Error("Invalid type");
+  }
 }
-const objOne = builder(2,"type2");
-const objTwo = builder(4,"type1");
+const objOne = builder(2, "type2");
+const objTwo = builder(4, "type1");
 
 console.log(objOne);
 console.log(objTwo);
