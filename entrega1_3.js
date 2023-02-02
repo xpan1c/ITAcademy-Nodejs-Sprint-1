76068e75f73292209f2eef32a87f615485f2f027
Danny Mejia <dannym.mejia@gmail.com>
2023-02-02 12:55:30 +0100

    entrega1_3 N1 Ex1 acabado

--- entrega1_3.js
+++ entrega1_3.js
@@ -1,4 +1,19 @@
 //Nivel 1
 //Ex 1.
-const promise =  new Promise((resolve, reject) => {
-});
+function a(variable){
+    return new Promise((resolve, reject) => {
+        if (typeof variable === 'string')  {
+            resolve("This argument is a String");
+        }else{
+            reject("This argument is not a String");
+        }
+    });
+}
+
+a(2)
+.then( message => console.log(message))
+.catch(error => console.log(error));
+
+a("a")
+.then( message => console.log(message))
+.catch(error => console.log(error));
