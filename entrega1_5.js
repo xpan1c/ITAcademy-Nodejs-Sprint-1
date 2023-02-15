9c536b6a101bd8363b4a63dc86c8837362ae5a6b
Danny Mejia <dannym.mejia@gmail.com>
2023-02-15 11:44:20 +0100

    entrega1_5.js N1 y N3 realizados

---
+++ entrega1_5.js
@@ -0,0 +1,54 @@
+// requires
+const fs = require('fs');
+const fsPromises = require('node:fs/promises');
+const path = require('path');
+const zlib = require('zlib');
+const {promisify} = require('node:util');
+const { pipeline } = require('node:stream');
+// Nivel 1
+// Ex 1
+
+const frase = "Hola mundo";
+const pt = 'test.txt';
+const writeFilePromise = (a,b) => fsPromises.writeFile(a,b);
+//Ex 2
+const encoding = 'utf8';
+const readFilePromise = (a,b) => fsPromises.readFile(a,b);
+//Ex 3
+const zipGen = (input) => {
+  const gzip = zlib.createGzip();
+  const source = fs.createReadStream(input);
+  const destination = fs.createWriteStream(input+'.gz');
+  const pipe = promisify(pipeline);
+  return pipe(source,gzip,destination)
+}
+// Async -await 
+
+const start = async () =>{
+  try {
+    await writeFilePromise(pt,frase);
+    const first = await readFilePromise(pt,encoding);
+    console.log(first);
+    await zipGen(pt);
+  } catch (error) {
+    console.log(error);
+  }
+}
+start();
+// Nivel 2
+// Ex 1
+const recursive = msj => {setTimeout(() => {
+  console.log(msj);
+  return recursive(msj)}, 1000);
+}
+recursive('Hola');
+// Ex 2
+const {spawn} = require('child_process')
+const child = spawn('dir /b', { shell: true });
+child.stdout.on('data', (data) => {
+  console.log(`\n${data}`);
+});
+
+child.stderr.on('data', (data) => {
+  console.error(`child stderr:\n${data}`);
+});
